0407f3d55d73a95ace4303d18876203f4ae38775
GHAJH <124276317+GHAJH@users.noreply.github.com>
2023-02-02 20:08:38 +0530

    added via desktop

---
+++ ghajh.cpp
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<iostream>
+#include<fstream>
+#include<cctype>
+#include<iomanip>
+int a;
+int openacc();
+int accbal();
+int fundtrs();
+int billpayrec();
+int chkbkcrd();
+int depacc();
+int loan();
+int inv();
+int sec();
+int filtax(); 
+int acccls();
+void intro()
+{
+	printf("\n\n\n\t  Online");
+	printf("\n\n\tBanking");
+	printf("\n\n\t  SYSTEM");
+	printf("\n\n\n\nMADE BY :\n1.Akshay Satya\n2.Himani Garige\n3.Jahnavi\n4.Harina suri\n5.Gautam kedia");
+	printf("\nUniversity :Keshav Memorial Institute Of Technology");
+	cin.get();
+}
+int main()
+{
+	int n;
+	intro();
+	do
+	 {
+	printf("\t\t Welcome to Online Banking System");
+	printf("\n\t 1.Open Account");
+	printf("\n\t ");
+	printf("\n\t ");
+	printf("\n\t ");
+	printf("\n\t ");
+	printf("\n\t ");
+	printf("\n\t ");
+	printf("\n\t ");
+	printf("\n\t ");
+	printf("\n\t ");
+	printf("\n\t ");
+     }
+while(n>0);
+}
+
